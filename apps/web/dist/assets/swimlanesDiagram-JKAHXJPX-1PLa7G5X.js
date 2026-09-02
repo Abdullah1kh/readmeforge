237619621171de2b663b65c048fea673c704fb36
Abdullah Khurram <abdullah.khurram00@gmail.com>
2026-09-02 11:35:54 +0500

    Add files via upload

---
+++ apps/web/dist/assets/swimlanesDiagram-JKAHXJPX-1PLa7G5X.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-T62WH6J4-iB_LW-Yk.js";import{_ as a}from"./mermaid.core-Dcm7ZUL6.js";import"./chunk-5VM5RSS4-CpbL7Vjl.js";import"./chunk-XXDRQBXY-Bgi3ZDLr.js";import"./chunk-POPQ4Y6H-2s_NEmjg.js";import"./chunk-F27PBJKO-CAqAw4TF.js";import"./channel-EINzKsWY.js";import"./index-BSQnULWN.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
